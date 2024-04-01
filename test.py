8ee9684e675d16edc86411c6d3cff25b755b83f8
vasiliy <vasiliy@fb.com>
2024-04-01 14:14:54 -0700

    add float8
    Summary:
    
    Test Plan:
    
    Reviewers:
    
    Subscribers:
    
    Tasks:
    
    Tags:

--- test.py
+++ test.py
@@ -3,16 +3,31 @@ import torch
 from fp_formats import _assert_equals, dtype_to_interesting_values
 
 def test_fp32():
-    interesting_values = dtype_to_interesting_values[torch.float]
-    for fp_ref, s_enc_ref, e_enc_ref, m_enc_ref, dtype, _notes in interesting_values:
+    dtype = torch.float
+    interesting_values = dtype_to_interesting_values[dtype]
+    for fp_ref, s_enc_ref, e_enc_ref, m_enc_ref, _notes in interesting_values:
         _assert_equals(fp_ref, s_enc_ref, e_enc_ref, m_enc_ref, dtype)
 
 def test_bf16():
-    interesting_values = dtype_to_interesting_values[torch.bfloat16]
-    for fp_ref, s_enc_ref, e_enc_ref, m_enc_ref, dtype, _notes in interesting_values:
+    dtype = torch.bfloat16
+    interesting_values = dtype_to_interesting_values[dtype]
+    for fp_ref, s_enc_ref, e_enc_ref, m_enc_ref, _notes in interesting_values:
         _assert_equals(fp_ref, s_enc_ref, e_enc_ref, m_enc_ref, dtype)
 
 def test_fp16():
-    interesting_values = dtype_to_interesting_values[torch.float16]
-    for fp_ref, s_enc_ref, e_enc_ref, m_enc_ref, dtype, _notes in interesting_values:
+    dtype = torch.float16
+    interesting_values = dtype_to_interesting_values[dtype]
+    for fp_ref, s_enc_ref, e_enc_ref, m_enc_ref, _notes in interesting_values:
+        _assert_equals(fp_ref, s_enc_ref, e_enc_ref, m_enc_ref, dtype)
+
+def test_float8_e4m3fn():
+    dtype = torch.float8_e4m3fn
+    interesting_values = dtype_to_interesting_values[dtype]
+    for fp_ref, s_enc_ref, e_enc_ref, m_enc_ref, _notes in interesting_values:
+        _assert_equals(fp_ref, s_enc_ref, e_enc_ref, m_enc_ref, dtype)
+
+def test_float8_e5m2():
+    dtype = torch.float8_e5m2
+    interesting_values = dtype_to_interesting_values[dtype]
+    for fp_ref, s_enc_ref, e_enc_ref, m_enc_ref, _notes in interesting_values:
         _assert_equals(fp_ref, s_enc_ref, e_enc_ref, m_enc_ref, dtype)
